6b5da6d463dd52a1a12824814a4c6ad569909061
Julian Blasco <blascoblasco9@gmail.com>
2020-04-09 13:54:29 -0300

    Create user and post model and migrate database

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 20 }
+  validates :email, presence: true
+  validates :password, presence: true
+
+end
